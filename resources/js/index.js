042708d2d8808781c5fe845fafcba65c0ce94376
Wannes Matthys <wannesmatthys@gmail.com>
2024-05-17 08:51:17 +0200

    update javascript

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,15 +1,32 @@
-// https://www.npmjs.com/package/diff
-const Diff = require('diff');
+const getStringsDifference = (str1, str2) => {
+  const lines1 = str1.split('\n');
+  const lines2 = str2.split('\n');
 
-const getStringsDifference = (str1, str2, method = 'diffLines', options = {}) => {
-  const result = Diff[method](str1, str2, options);
+  let i = 0;
+  let j = 0;
+  const result = [];
 
-  return result
-    .map((part) => {
-      const color = part.added ? 'green' : part.removed ? 'red' : 'grey';
-      return `<span style="color: ${color}">${part.value}</span>`;
-    })
-    .join('');
+  while (i < lines1.length || j < lines2.length) {
+    if (i < lines1.length && j < lines2.length && lines1[i] === lines2[j]) {
+      // Same line in both strings
+      result.push(`<span style="color: grey">${lines1[i]}</span>`);
+      i++;
+      j++;
+    } else {
+      if (i < lines1.length) {
+        // Line removed
+        result.push(`<span style="color: red">${lines1[i]}</span>`);
+        i++;
+      }
+      if (j < lines2.length) {
+        // Line added
+        result.push(`<span style="color: green">${lines2[j]}</span>`);
+        j++;
+      }
+    }
+  }
+
+  return result.join('<br>');
 };
 
 window.getStringsDifference = getStringsDifference;
